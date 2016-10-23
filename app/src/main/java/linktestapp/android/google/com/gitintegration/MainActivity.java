8553bfcf085c4c83ba77d8596fe1cc0286340e19
rosca.razvan <rosca.razvan@rocketmail.com>
2016-10-23 16:18:19 +0300

    [v1.0] First commint

---
+++ app/src/main/java/linktestapp/android/google/com/gitintegration/MainActivity.java
@@ -0,0 +1,16 @@
+package linktestapp.android.google.com.gitintegration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "`zc`zc", Toast.LENGTH_SHORT).show();
+    }
+}
